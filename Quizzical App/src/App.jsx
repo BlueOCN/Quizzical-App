e975f3799fd96e109634cc0c1b5443a006ffac5b
BlueOCN <robgonzalezmu@gmail.com>
2024-08-29 19:37:22 -0600

    Blobs added & Layout update

--- Quizzical App/src/App.jsx
+++ Quizzical App/src/App.jsx
@@ -10,12 +10,33 @@ function App() {
   const [quizzing, setQuizzing] = useState(false)
   const [check, setCheck] = useState(false)
   const [allQuestions, setAllQuestions] = useState([])
+  const [score, setScore] = useState(0)
 
-  useEffect(() => {
+  /* useEffect(() => {
       fetch("https://opentdb.com/api.php?amount=5&type=multiple")
           .then(res => res.json())
           .then(resData => setAllQuestions(extractQuizz(resData)))
-  }, [])
+  }, [quizzing]) */
+
+  useEffect(() => {
+    const fetchQuestions = async () => {
+        try {
+            const response = await fetch("https://opentdb.com/api.php?amount=5&type=multiple");
+            
+            // Check if the response is okay (status code 200-299)
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            
+            const resData = await response.json();
+            setAllQuestions(extractQuizz(resData));
+        } catch (error) {
+            console.error("Error fetching questions:", error);
+            // Handle the error, e.g., set an error state or show a message
+        }
+    };
+    fetchQuestions();
+  }, [quizzing]);
 
   function shuffleAnswers(answers) {
     for (let i = answers.length - 1; i > 0; i--) {
@@ -26,67 +47,152 @@ function App() {
   }
 
   function extractQuizz(apiData) {
-    if (apiData === 0) {
-      return []
-    }
+
     const questionArray = []
     for (let index = 0; index < apiData.results.length; index++) {
-      const r_answer = he.decode(apiData.results[index].correct_answer)
-      const w_answers = apiData.results[index].incorrect_answers.map(ans => he.decode(ans))
+      
+      const answersArray = []
+      answersArray.push({
+        id: nanoid(), 
+        text: he.decode(apiData.results[index].correct_answer), 
+        style: 0, 
+        isSelected: false, 
+        isRight: true
+      })
+
+      const in_ans = apiData.results[index].incorrect_answers.map(ans =>{
+        return (
+          {
+            id: nanoid(), 
+            text: he.decode(ans), 
+            style: 0, 
+            isSelected: false, 
+            isRight: false
+          }
+        )
+      })
 
       questionArray.push({
         id: nanoid(),
         question: he.decode(apiData.results[index].question), 
-        answers: shuffleAnswers(w_answers.concat(r_answer))
+        answers: shuffleAnswers(answersArray.concat(in_ans))
       })
     }
     return questionArray
   }
 
+
   function startQuizzing() {
+    setScore(0)
+    setCheck(false)
     setQuizzing(prevQuizzing => !prevQuizzing)
   }
 
-  function checkAnswers() {
-    setCheck(prevCheck => ! prevCheck)
+  console.log(allQuestions)
+
+  /* function checkAnswers() {
+    setCheck(prevCheck => !prevCheck)
+    setAllQuestions(prevQuestions => prevQuestions.map(prevQuestion => {
+      const newAnswers = prevQuestion.answers.map(prevAnswer => {
+        // console.log(prevAnswer.isRight, prevAnswer.isSelected)
+        if(prevAnswer.isRight){
+          // console.log("style 3", prevAnswer.style)
+          return ({...prevAnswer, style: 3})
+        } else if(prevAnswer.isSelected && prevAnswer.isRight !== prevAnswer.isSelected) {
+          // console.log("style 4", prevAnswer.style)
+          return ({...prevAnswer, style: 4})
+        } else {
+          // console.log("style 2", prevAnswer.style)
+          return ({...prevAnswer, style: 2})
+        }
+      })
+      // console.log("Q is right?", newAnswers.every((ans)=> ans.isRight === ans.isSelected))
+      // const answerState = newAnswers.every((ans)=> ans.isRight === ans.isSelected)
+      // console.log("Q is right?", answerState)
+      
+      return ({...prevQuestion, answers: newAnswers})
+    }))
+  } */
+
+  const checkAnswers = () => {
+    let newScore = 0;
+    setCheck(prev => !prev);
+    setAllQuestions(prevQuestions => {
+      return prevQuestions.map(question => {
+        const newAnswers = question.answers.map(answer => {
+          if (answer.isRight) {
+            if (answer.isSelected) {
+              newScore++;
+            }
+            return { ...answer, style: 3 }; // Correct answer, highlighted
+          }
+          return { ...answer, style: answer.isSelected ? 4 : 2 }; // Incorrect or unselected
+        });
+        return { ...question, answers: newAnswers };
+      });
+    });
+    setScore(newScore);
+  };
+
+
+  function selectAnswer(questionId, answerId){
+    if(!check){
+      setAllQuestions(prevQuestions => prevQuestions.map(prevQuestion => {
+
+        if(prevQuestion.id === questionId){
+          const newAnswers = prevQuestion.answers.map((prevAnswer) => {
+
+            if(prevAnswer.id === answerId){
+              return ({...prevAnswer, style: 1, isSelected: true})
+            }
+            else {
+              return ({...prevAnswer, style: 0, isSelected: false})
+            }
+          })
+
+          return ({...prevQuestion, answers: newAnswers})
+        }
+        return ({...prevQuestion})
+      })
+    )}
   }
 
-  useEffect(() => {
-    console.log("All questions changed")
-  }, [allQuestions])
-
-  const questionElements = allQuestions.map((q) => (
+  const questionElements = allQuestions.map((prevQuestion) => (
     <Question 
-      key={q.id} 
-      query={q.question} 
-      shuffledAnswers={q.answers}
+      key={prevQuestion.id}
+      id={prevQuestion.id}
+      query={prevQuestion.question} 
+      answers={prevQuestion.answers}
+      selectAnswer={selectAnswer}
     />
   ))
 
   return (
     <>
-      {quizzing ? 
+      <img className="blob1" src="./Blob1.svg" alt="blob1" />
+      <img className="blob2" src="./Blob2.svg" alt="blob2" />
+      { quizzing ? 
       <main className='quizz'>
         <div className='quizz-container'>
-          {questionElements}
+          { questionElements }
         </div>
 
         <div className='button-container'>
-          { check && <h2 className='results'>You scored #/5 correct answers</h2>}
-          <button 
-            className='quizz-button'
-            onClick={checkAnswers}
-          >
-            {check ? 'Play again' : 'Check answers'}
-          </button>
+          { check && <h2 className='results'>You scored {score}/5 correct answers</h2> }
+          
+          { check ?
+            <button className='quizz-button' onClick={() => startQuizzing()}>Play again</button>
+            :
+            <button className='quizz-button' onClick={() => checkAnswers()}>Check answers</button>
+          }
         </div>
         
       </main> 
       : 
       <main className='menu'>
         <h1 className='menu-title'>Quizzical</h1>
-        <p className='menu-info'>Some description if needed</p>
-        <button className='menu-button' onClick={startQuizzing}>Start quiz</button>
+        <p className='menu-info'>If you know you know what you know know ...</p>
+        <button className='menu-button' onClick={() => startQuizzing()}>Start quiz</button>
       </main>
       }
     </>
--- Quizzical App/src/components/Question.jsx
+++ Quizzical App/src/components/Question.jsx
@@ -1,24 +1,59 @@
 import PropTypes from 'prop-types'
 
 function Question(props) {
-    return (
-        <div className='question-container'>
-            <h1 className='query'>{props.query}</h1>
-            <div className='answers'>
-              <button className='answers-button'>{props.shuffledAnswers[0]}</button>
-              <button className='answers-button'>{props.shuffledAnswers[1]}</button>
-              <button className='answers-button'>{props.shuffledAnswers[2]}</button>
-              <button className='answers-button'>{props.shuffledAnswers[3]}</button>
-            </div>
-            <br />
-          </div>
-    )
+
+  function getStyle(code) {
+    var style
+    switch(code) {
+      case 1:
+        style = "button-selected-blue"
+        break;
+
+      case 2:
+        style = "button-faded-blue"
+        break;
+      
+      case 3:
+        style = "button-green"
+        break;
+
+      case 4:
+        style = "button-faded-red"
+        break;
+      
+      default:
+        style = "button-blue"
+    }
+    return style
+  }
+
+  const answerElements = props.answers.map((answer) => (
+    <button 
+      key={answer.id} 
+      className={`answers-button ${getStyle(answer.style)}`}
+      onClick={() => props.selectAnswer(props.id, answer.id)}
+      >{answer.text}
+    </button>
+  ))
+  // console.log(answerElements)
+
+  return (
+    <div className='question-container'>
+      <h1 className='query'>{props.query}</h1>
+      <div className='answers'>
+        {answerElements}
+      </div>
+      <br />
+    </div>
+  )
 }
 
 export default Question
 
 Question.propTypes = {
   key: PropTypes.string,
+  id: PropTypes.string,
   query: PropTypes.string,
-  shuffledAnswers: PropTypes.array
+  answers: PropTypes.array,
+  selectAnswer: PropTypes.func
 }
